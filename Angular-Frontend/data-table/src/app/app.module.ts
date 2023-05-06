1e978c9efbd15b8659c16b8a95d9e95659622685
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2023-05-06 18:15:23 +0300

    setup npm/ng versions and install prime ng

--- Angular-Frontend/data-table/src/app/app.module.ts
+++ Angular-Frontend/data-table/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { GetUsersComponent } from './modules/users/pages/get-users/get-users.component';
 import { HttpClientModule } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+
+import { AppComponent } from './app.component';
+import { GetUsersComponent } from './modules/users/pages/get-users/get-users.component';
 import { NavigationBarComponent } from './shared/pages/navigation-bar/navigation-bar.component';
 import { FooterComponent } from './shared/pages/footer/footer.component';
 
@@ -19,7 +21,8 @@ import { FooterComponent } from './shared/pages/footer/footer.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
