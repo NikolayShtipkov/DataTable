1ff9102861f1da5390394225702a9b76e4b57945
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2023-05-06 16:05:41 +0300

    create navigation bar and footer components

--- Angular-Frontend/data-table/src/app/app.module.ts
+++ Angular-Frontend/data-table/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { GetUsersComponent } from './modules/users/pages/get-users/get-users.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavigationBarComponent } from './shared/pages/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './shared/pages/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavigationBarComponent,
+    FooterComponent,
     GetUsersComponent,
   ],
   imports: [
