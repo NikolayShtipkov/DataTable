31478999bb519070b8ef0f3e9fd9e736ca5dbc81
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2023-05-06 15:46:56 +0300

    separate html from app.component

--- Angular-Frontend/data-table/src/app/app.component.ts
+++ Angular-Frontend/data-table/src/app/app.component.ts
@@ -2,21 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to Niko's {{title}}.
-      </h1>
-      <span style="display: block">{{ title }} app is running.</span>
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a routerLink="/users">Go To Users</a></h2>
-    </ul>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
